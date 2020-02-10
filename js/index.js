06b26c14219f4957f66071078f14a0724a2521d6
w-shuang <1761731525@qq.com>
2020-02-10 15:49:17 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var oUl=document.getElementById("login");
+var aLi=oUl.getElementsByTagName("li");
+aLi[8].onclick = function(){
+    window.location.href="login2.html"
+}
---
+++ js/login.js
@@ -0,0 +1,4 @@
+$(".btn").click(function(){
+    // $(".msg").hide();
+    $(".msg").css('display','none');//隐藏
+})
